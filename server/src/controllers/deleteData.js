0d9c302347bcf1d17f9850f864a7fca483202ed7
TanjimAnim <tanjimanim007@gmail.com>
2022-10-22 19:39:27 +0600

    feat: api to delete a single book

---
+++ server/src/controllers/deleteData.js
@@ -0,0 +1,40 @@
+//api to get books
+const pool = require("../configs/db");
+
+const deleteData = async (req, res, next) => {
+  try {
+    const { email, book_id } = req.body;
+
+    if (
+      book_id === "" ||
+      book_id === undefined ||
+      typeof book_id !== "number"
+    ) {
+      throw new Error("Error in book");
+    }
+
+    const existingUser = await pool.query(
+      `SELECT email,user_id FROM accounts WHERE email= $1;`,
+      [email]
+    );
+
+    const getBookData = await pool.query(
+      `SELECT * FROM books WHERE book_id=$1 AND user_id=$2`,
+      [book_id, existingUser.rows[0].user_id]
+    );
+
+    if (getBookData.rows.length === 0) {
+      throw new Error("book not found");
+    } else {
+      await pool.query(`DELETE FROM books WHERE book_id=$1 AND user_id=$2`, [
+        book_id,
+        existingUser.rows[0].user_id,
+      ]);
+      res.json({ message: "Book deleted" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = deleteData;
