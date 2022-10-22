4363c0200050287b181e7dcb84942b747eaf59fe
TanjimAnim <tanjimanim007@gmail.com>
2022-10-22 19:39:15 +0600

    feat: api to add books to the list

---
+++ server/src/controllers/addData.js
@@ -0,0 +1,65 @@
+//api to add books
+
+const pool = require("../configs/db");
+
+//slug package for making book name slugs
+var slug = require("slug");
+
+const addData = async (req, res, next) => {
+  try {
+    const { email, book_name, year, book_author } = req.body;
+
+    //checking input
+    if (
+      book_name === undefined ||
+      book_name === "" ||
+      typeof book_name !== "string"
+    ) {
+      throw new Error("Error in book name");
+    }
+    if (year === undefined || year === "" || typeof year !== "number") {
+      throw new Error("Error in year");
+    }
+    if (
+      book_author === undefined ||
+      book_author === "" ||
+      typeof book_author !== "string"
+    ) {
+      throw new Error("Error in author");
+    }
+    const book_slug = slug(`${book_name}`);
+    console.log(book_slug);
+
+    const existingUser = await pool.query(
+      `SELECT email,user_id FROM accounts WHERE email= $1;`,
+      [email]
+    );
+
+    console.log(existingUser.rows);
+
+    if (existingUser.rows.length === 0) {
+      throw new Error("user not found");
+    } else {
+      pool.query(
+        `INSERT INTO books(book_name, year, book_author,user_id,book_slug) VALUES ($1,$2,$3,$4,$5);`,
+        [book_name, year, book_author, existingUser.rows[0].user_id, book_slug],
+        (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({
+              error: "Database error",
+            });
+          } else {
+            res.status(200).send({
+              message: "Book added",
+            });
+          }
+        }
+      );
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = addData;
