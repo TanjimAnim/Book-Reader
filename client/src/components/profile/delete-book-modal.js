1b992d9fb805f4cfb6fb9c21473124eb24982ca9
TanjimAnim <tanjimanim007@gmail.com>
2022-10-23 17:58:38 +0600

    feat:modal for book deletion

---
+++ client/src/components/profile/delete-book-modal.js
@@ -0,0 +1,75 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Button,
+} from "@chakra-ui/react";
+import axios from "axios";
+
+import React from "react";
+
+import { useAuthContext } from "../../context/authcontext";
+
+function DeleteBookModal({ isOpen, onClose, onSuccessfulUpload, id }) {
+  const { getToken } = useAuthContext();
+  const token = getToken();
+  const deleteBook = async () => {
+    axios
+      .post("http://localhost:5000/delete-book", {
+        token: `${token}`,
+        book_id: `${id}`,
+      })
+      .then((response) => {
+        console.log(response);
+        alert(`${response.data.message}`);
+        onSuccessfulUpload();
+      })
+      .catch((error) => {
+        console.log(error.response.statusText);
+        alert(`${error.response.statusText}`);
+      });
+  };
+  const cancelRef = React.useRef();
+  return (
+    <>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete Book
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  deleteBook();
+                  onClose();
+                }}
+                ml={3}
+              >
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  );
+}
+
+export default DeleteBookModal;
