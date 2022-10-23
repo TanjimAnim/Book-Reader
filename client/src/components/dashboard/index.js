3c8fbbc39cd317732f9f9c440545dd73e54277f1
TanjimAnim <tanjimanim007@gmail.com>
2022-10-23 17:57:12 +0600

    feat: refresh books list on delete/add

--- client/src/components/dashboard/index.js
+++ client/src/components/dashboard/index.js
@@ -1,15 +1,44 @@
 import { Box } from "@chakra-ui/react";
+import { useEffect, useCallback, useState } from "react";
 
+import axios from "axios";
+import { useAuthContext } from "../../context/authcontext";
 //importing components
 import Navbar from "../navbar";
 import Profile from "../profile";
 
 export default function Dashboard() {
+  const [listOfBooks, setListOfBooks] = useState([]);
+  const { getToken } = useAuthContext();
+  const token = getToken();
+  const refreshBooksList = useCallback(() => {
+    axios
+      .get("http://localhost:5000/get-books", {
+        headers: {
+          "x-access-token": `${token}`,
+        },
+      })
+      .then(function (response) {
+        console.log(response.data);
+        setListOfBooks(response.data);
+      })
+      .catch(function (error) {
+        console.log(error.response);
+      });
+  }, []);
+
+  useEffect(() => {
+    refreshBooksList();
+  }, [refreshBooksList]);
+
   return (
     <>
-      <Box height="100vh" background="#F9F9F9">
+      <Box minHeight="100vh" background="#F9F9F9">
         <Navbar />
-        <Profile />
+        <Profile
+          listOfBooks={listOfBooks}
+          onSuccessfulUpload={refreshBooksList}
+        />
       </Box>
     </>
   );
